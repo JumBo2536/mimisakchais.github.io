4655f120947a856ec76cbb118fc116e17850ccbe
JumBo88 <sakchais55@nu.ac.th>
2016-01-20 13:26:44 +0700

    fff

--- index.php
+++ index.php
@@ -15,66 +15,7 @@
 
 </head>
 <body material fluid >
-<?php
-header('Access-Control-Allow-Origin: http://10.27.68.38');
-echo $json;
-?>
-<script type="text/javascript">
-
-$.ajax({
-    type: 'GET',
-    url: 'http://10.27.68.38/meter/api.php?key=USA8DD2D&type=selectdate&addr=1',
-    dataType: 'jsonp',
-    success: function(data) {
-        console.log(data);
-    }
-});
-
-	/* $.getJSON('http://10.27.68.38/meter/api.php?key=USA8DD2D&type=selectdate&addr=1', function(result,status)
-	{
-		console.log(" => "+result);				
-								
-	});
-	console.log(" => test");  */
-	
-	
-	
-</script>
-	<div toolbar seamed bg-Grey500>
-		
-		<font color="white"><center><h2> test</h2></center></font>
-		
-		
-	</div>
-	
-	
-<div bg-grey100="" padded="">
-	
-	<div content>
-      <div fluid card bg-Orange500="">
-	  
-	  
-	 <center>
-		<div card="" z-2="" >
-		
-	gggggg
-		</div>
-		
-		</center>
-	
-        
-		
-    </div>
-	
-	</div>
-
-	
-</div>
-
-
-	
-	
-	
+hhhhhhhh
 	<!-- load scripts at the end -->
 
   <script src="js/zepto.min.js"></script>
